a687534340ae7bac89fc2f9c2aea3e59c085365d
Kishan <92583766+kishan1000@users.noreply.github.com>
2023-04-18 07:43:35 +0530

    from pc

--- emart/urls.py
+++ emart/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 from django.views.static import serve
-from django.conf.urls import url
+# from django.conf.urls import url
+from django.urls import re_path
 from django.conf import settings
 
 urlpatterns = [
@@ -10,6 +11,8 @@ urlpatterns = [
     path('', include('app.urls')),
     path('accounts/',include('django.contrib.auth.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
